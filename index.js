d0cc21a7171d999b316e786f2efa611f4dc2e66e
ravi9879 <144209099+ravi9879@users.noreply.github.com>
2024-02-04 17:56:02 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,51 +13,51 @@ const PORT = 800 // process.env.PORT || ;
 // app.use(dy.urlencoded({ extended: true })); 
 
 
-// function is_safe( r , c , oard , v ) {
-//     let e = 0 , f = 0 ;
-//     for(let i = 0 ; i < 9 ; i++){
-//         if(oard[r][i] === v){
-//             return false ;
-//         }
-//         if(oard[i][c] === v){
-//             return false ;
-//         }
-//         e = 3*(Math.floor(r/3)) + Math.floor(i/3) ;
-//         f = 3*(Math.floor(c/3)) + Math.floor(i%3)  ;
+function is_safe( r , c , oard , v ) {
+    let e = 0 , f = 0 ;
+    for(let i = 0 ; i < 9 ; i++){
+        if(oard[r][i] === v){
+            return false ;
+        }
+        if(oard[i][c] === v){
+            return false ;
+        }
+        e = 3*(Math.floor(r/3)) + Math.floor(i/3) ;
+        f = 3*(Math.floor(c/3)) + Math.floor(i%3)  ;
         
-//         if(oard[e][f] === v){
-//             return false ;
-//         } 
-//     } 
-//     return true ;
+        if(oard[e][f] === v){
+            return false ;
+        } 
+    } 
+    return true ;
     
-// }
+}
 
 
 
-// function suduko_solver( oard ){
-//     let n = 9;
-//     for(let r = 0 ; r < n ; r++){
-//         for(let c = 0 ; c < n ; c++ ){
-//             if(oard[r][c] === 0){
-//                 for(let v = 1 ; v <= 9 ; v++){
-//                     if(is_safe(r,c, oard ,v)){
-//                         oard[r][c] = v ; 
-//                         const as = suduko_solver(oard);
-//                         if(as){
-//                             return true ;
-//                         }
-//                         else {
-//                             oard[r][c] = 0 ;
-//                         }
-//                     }
-//                 }
-//                 return false ;
-//             }
-//         }
-//     }
-//     return true ;
-// }
+function suduko_solver( oard ){
+    let n = 9;
+    for(let r = 0 ; r < n ; r++){
+        for(let c = 0 ; c < n ; c++ ){
+            if(oard[r][c] === 0){
+                for(let v = 1 ; v <= 9 ; v++){
+                    if(is_safe(r,c, oard ,v)){
+                        oard[r][c] = v ; 
+                        const as = suduko_solver(oard);
+                        if(as){
+                            return true ;
+                        }
+                        else {
+                            oard[r][c] = 0 ;
+                        }
+                    }
+                }
+                return false ;
+            }
+        }
+    }
+    return true ;
+}
 
 
 
