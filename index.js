10769e73ecac955067868f8eaadcd8b55724044d
ravi9879 <ravigupta77389@gmail.com>
2024-02-04 18:02:49 +0530

    ans

--- index.js
+++ index.js
@@ -1,21 +1,16 @@
 const express = require('express');
 // const cp = require('cookie-parser');
 // const cors = require('cors');
-// const dy = require('body-parser');
+const dy = require('body-parser');
 const app = express();
 const PORT = 800 // process.env.PORT || ;
 
 
 
 // app.use(cp());
-// app.use(cors(
-//     {
-//         origin: ["http://localhost:800/"],
-//         methods: ["POST", "GET"],
-//         credentials: true
-//     }));
-// app.use(dy.json());
-// app.use(dy.urlencoded({ extended: true })); 
+// app.use(cors());
+app.use(dy.json());
+app.use(dy.urlencoded({ extended: true })); 
 
 
 function is_safe( r , c , oard , v ) {
@@ -85,6 +80,16 @@ app.post('/ans' , (req,res)=> {
 
 
 
+app.get('/ans' , (req,res)=> {
+    const array= [[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0],[0,0,0,0,0,0,0,0,0]];
+    // console.log(array) ;    
+
+    if( suduko_solver(array)){
+        // console.log("error") ;
+        res.send(array) ;
+    } 
+ 
+})
 
 app.listen(PORT, () => {
     console.log('app started successsfuly');
