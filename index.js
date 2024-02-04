536aadd0c442844163d409c6521dd117932e5f37
ravi9879 <144209099+ravi9879@users.noreply.github.com>
2024-02-04 14:31:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const express = require('express');
-// const cp = require('cookie-parser');
-// const cors = require('cors');
+const cp = require('cookie-parser');
+const cors = require('cors');
 const dy = require('body-parser');
 const app = express();
 const PORT = 800 // process.env.PORT || ;
 
 
 
-// app.use(cp());
-// app.use(cors(
-//     {
-//         origin: ["http://localhost:800/"],
-//         methods: ["POST", "GET"],
-//         credentials: true
-//     }));
+app.use(cp());
+app.use(cors());
 app.use(dy.json());
 app.use(dy.urlencoded({ extended: true })); 
 
